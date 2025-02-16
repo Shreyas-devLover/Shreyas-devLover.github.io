70ab4f59cc23ca262eee4966e7c53c7e2a8a5ec1
JakeTalo <140406613+JakeTalo@users.noreply.github.com>
2025-02-16 16:33:46 +0530

    restructured front end finalized

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function next1(event) {
     if (!document.getElementById("health").value || !(document.getElementById("Vegetarian").checked || document.getElementById("Non-Vegetarian").checked)) return
     event.preventDefault()
     if (document.getElementById("Vegetarian").checked) {
-        document.getElementById("sec4").style.display = "block"
+        document.getElementById("sec5").style.display = "block"
         document.getElementById("sec1").style.display = "none"
         setTimeout(window.close, 1000 * 10)
     } else {
@@ -11,21 +11,46 @@ function next1(event) {
     }
 }
 
-let veg = true
+let veg = false
 function selectVeg() {
     veg = true
 }
 function selectNonVeg() {
     veg = false
 }
+let sec2clicked=false
+function sec2click(){
+    sec2clicked=true;
+}
 function next2() {
+    if(!sec2clicked) {
+        window.alert("Please select a field")
+        return
+    }
     document.getElementById("sec2").style.display = 'none'
     document.getElementById("sec3").style.display = "block"
-    if (veg) document.getElementById("sec3_1").style.display = "block"
-    else document.getElementById("sec3_2").style.display = "block"
 }
-function submit() {
-        document.getElementById("sec4").style.display = "block"
-        document.getElementById("sec3").style.display = "none"
+let sec3clicked=false
+function sec3click(){
+    sec3clicked=true;
+}
+function next3() {
+    if(!sec3clicked) {
+        window.alert("Please select a field")
+        return
+    }
+    document.getElementById("sec3").style.display = 'none'
+    document.getElementById("sec4").style.display = "block"
+    if (veg) document.getElementById("sec4_1").style.display = "block"
+    else document.getElementById("sec4_2").style.display = "block"
+}
+function submitos(event) {
+    if ((document.getElementById("textarea1").value || document.getElementById("textarea2").value) &&
+        (document.getElementById("whychoice1").value || document.getElementById("whychoice2").value)) {
+        event.preventDefault()
+        document.getElementById("sec5").style.display = "block"
+        document.getElementById("sec4").style.display = "none"
         setTimeout(window.close, 1000 * 10)
+    }
+    else return
 }
