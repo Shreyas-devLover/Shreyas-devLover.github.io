aa5b92622f1d7ed34d687200bc8141a9ae178766
JakeTalo <140406613+JakeTalo@users.noreply.github.com>
2025-02-16 22:54:04 +0530

    added api integration

--- index.js
+++ index.js
@@ -5,43 +5,49 @@ function next1(event) {
         document.getElementById("sec5").style.display = "block"
         document.getElementById("sec1").style.display = "none"
         setTimeout(window.close, 1000 * 10)
+        fetch("http://localhost:3000/survey", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=UTF-8"
+            },
+            body: JSON.stringify({
+                veg: true
+            })
+        })
     } else {
         document.getElementById("sec1").style.display = 'none'
         document.getElementById("sec2").style.display = "block"
     }
 }
 
-let veg = false
-function selectVeg() {
-    veg = true
+let type = ''
+function selectType(arg_type) {
+    type = arg_type
 }
-function selectNonVeg() {
-    veg = false
-}
-let sec2clicked=false
-function sec2click(){
-    sec2clicked=true;
+let sec2clicked = ''
+function sec2click(category) {
+    sec2clicked = category;
 }
 function next2() {
-    if(!sec2clicked) {
+    if (!sec2clicked) {
         window.alert("Please select a field")
         return
     }
     document.getElementById("sec2").style.display = 'none'
     document.getElementById("sec3").style.display = "block"
 }
-let sec3clicked=false
-function sec3click(){
-    sec3clicked=true;
+let sec3clicked = false
+function sec3click() {
+    sec3clicked = true;
 }
 function next3() {
-    if(!sec3clicked) {
+    if (!sec3clicked) {
         window.alert("Please select a field")
         return
     }
     document.getElementById("sec3").style.display = 'none'
     document.getElementById("sec4").style.display = "block"
-    if (veg) document.getElementById("sec4_1").style.display = "block"
+    if (type) document.getElementById("sec4_1").style.display = "block"
     else document.getElementById("sec4_2").style.display = "block"
 }
 function submitos(event) {
@@ -50,7 +56,28 @@ function submitos(event) {
         event.preventDefault()
         document.getElementById("sec5").style.display = "block"
         document.getElementById("sec4").style.display = "none"
+        sendServer()
         setTimeout(window.close, 1000 * 10)
     }
     else return
+}
+function sendServer() {
+    fetch("http://localhost:3000/survey", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+        },
+        body: JSON.stringify({
+            veg: false,
+            data: {
+                argreeability: document.getElementById('health').value,
+                category: sec2clicked,
+                type,
+                review: document.getElementById("textarea1").value || document.getElementById("textarea2").value,
+                whychoice: document.getElementById("whychoice1").value || document.getElementById("whychoice2").value,
+            },
+            chicken_choosen: type ? true : false
+
+        })
+    })
 }
